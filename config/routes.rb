88e07ac1267b9102ebf6dd7512c1720509e1a854
yakko-chn <o.ub5ryno16i@gmail.com>
2020-10-12 22:50:42 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   resources :users, only: [:edit, :update]
   resources :sessions, only: [:destroy]
 
-  resources :rooms, only: [:new,:create]
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
 end
